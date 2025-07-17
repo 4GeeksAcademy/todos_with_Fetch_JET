cb4e50bd141d5fe5ad2004731c6759e72ab82f27
JoseEnri <jose.tocasuche@gmail.com>
2025-07-17 00:57:59 +0000

    work in progress

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,65 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  let [userName, setUserName] = useState("jet007");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    createUser(userName);
+    getUsersTodoList();
+    console.log(userName);
+  }, []);
+
+//creating the New User Function
+  function createUser(username) {
+    fetch("https://playground.4geeks.com/todo/users/" + username, {
+      method: "POST",
+      body: JSON.stringify([]),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+//creating the Get-to-do list Function
+function getUsersTodoList() {
+    fetch("https://playground.4geeks.com/todo/users/" + userName)
+    .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });    
+  }
+
+  return (
+    <div className="text-center">
+      <h1>To do list with Fetch</h1>
+      <input
+        type="text"
+        className="form-control me-3"
+        placeholder="Add your To Do and hit Enter or click the button"
+      />
+      <button className="btn btn-primary">Add to list</button>
+      <ul>
+        <li>Todo 1</li>
+        <li>Todo 2</li>
+        <li>Todo 3</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
